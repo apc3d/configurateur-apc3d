6d315c461f00c2f9d04060cffd668c88036ad170
apc3d <jimmy.lombard@apc3d.fr>
2025-04-22 17:32:06 +0200

    Delete script (1).js

--- script (1).js
+++
@@ -1,8 +0,0 @@
-// Pour changer de couleur sur sélection délai
-const deliveryButtons = document.querySelectorAll('.delivery-btn');
-deliveryButtons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    deliveryButtons.forEach(b => b.classList.remove('selected'));
-    btn.classList.add('selected');
-  });
-});
